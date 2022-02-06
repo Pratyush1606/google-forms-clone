dcebc7664580d91494c5da92caee882c6c57ef12
Pratyush1606 <jaiswalpratyush2015@gmail.com>
2022-02-07 01:28:58 +0530

    Added FormEntry and FormFieldAnswers models and serializers

--- forms_app/models.py
+++ forms_app/models.py
@@ -1,4 +1,3 @@
-from statistics import mode
 from django.db import models
 
 # Create your models here.
@@ -17,6 +16,15 @@ class FormField(models.Model):
     class Meta:
         unique_together = ('field_name', 'form_template',)
 
+class FormEntry(models.Model):
+    form_entry_id = models.BigAutoField(primary_key=True, editable=False)
+    form_template = models.ForeignKey(to=FormTemplate, related_name="form_entries", on_delete=models.CASCADE)
 
+class FormFieldAnswer(models.Model):
+    answer = models.CharField(max_length=100, null=True)
+    form_entry = models.ForeignKey(to=FormEntry, related_name="form_answers", on_delete=models.CASCADE)
+    form_field = models.ForeignKey(to=FormField, related_name="form_field_answers", on_delete=models.CASCADE)
 
-
+    class Meta:
+        unique_together = ('form_entry', 'form_field',)
+        
--- forms_app/serializers.py
+++ forms_app/serializers.py
@@ -1,6 +1,6 @@
 from ast import For
 from rest_framework import serializers
-from forms_app.models import FormTemplate, FormField
+from forms_app.models import FormTemplate, FormField, FormEntry, FormFieldAnswer
 
 class FormTemplateSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,4 +11,14 @@ class FormFieldSerializer(serializers.ModelSerializer):
     class Meta:
         model = FormField
         fields = ['field_id', 'field_name', 'type', 'options', 'mandatory', 'form_template']
+
+class FormEntrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FormEntry
+        fields = ['form_entry_id', 'form_template']
+
+class FormFieldAnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FormFieldAnswer
+        fields = ['answer', 'form_entry', 'form_field']
 

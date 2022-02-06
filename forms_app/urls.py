8a4bf4de7847f3dd5e35c4327f3b0c7f38e3678d
Pratyush1606 <jaiswalpratyush2015@gmail.com>
2022-02-06 23:39:41 +0530

    Added forms_app django app

---
+++ forms_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from forms_app import views
+
+app_name = 'forms_app'
+urlpatterns = [
+    
+]
--- google_forms/urls.py
+++ google_forms/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('forms_app.urls', namespace='forms_app')),
+    path('', include('forms_app.urls', namespace='forms_app')),
 ]
